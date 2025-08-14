5192d020a9dc97cbb503ca6f801a0f1fdd9b53a8
Alexander Masko <117868235+AlexOksam@users.noreply.github.com>
2025-08-14 23:46:09 +0200

    Update Bandit.sh

--- Bandit.sh
+++ Bandit.sh
@@ -19,5 +19,16 @@ alex@alex-Virtual-Machine:~$ ssh bandit0@bandit.labs.overthewire.org -p 2220
 
 bandit0@bandit.labs.overthewire.org's password:'
 
-bandit0@bandit: logout
-alex@alex-Virtual-Machine:~$
+bandit0@bandit:~$ ls
+readme
+bandit0@bandit:~$ cat readme 
+Congratulations on your first steps into the bandit game!!
+Please make sure you have read the rules at https://overthewire.org/rules/
+If you are following a course, workshop, walkthrough or other educational activity,
+please inform the instructor about the rules as well and encourage them to
+contribute to the OverTheWire community so we can keep these games free!
+
+The password you are looking for is: ZjLjTmM6FvvyRnrb2rfNWOZOTa6ip5If
+
+bandit0@bandit:~$
+
